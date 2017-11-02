303874e7e6416618d1c789fdcd31da3ff6263411
Lars Martin S. Pedersen <larsmsp@gmail.com>
2017-11-02 11:27:40 +0100

    Made intro assignment a bit more difficult

--- main.py
+++ main.py
@@ -22,13 +22,14 @@ def calculator():
     """
     Operatorer og operand er angitt i query-parametrene "operator", "operand1" og "operand2", 
     f.eks /calculator?operator=plus&operand1=2&operand2=2
+    Verdiene kan hentes ut av objektet request.args
     Endepunktet skal støtte "plus", "minus", "mult" og "div".
     :return: Resultatet av enkle heltallsoperasjoner.
     """
-    operand1 = int(request.args['operand1'])
-    operand2 = int(request.args['operand2'])
-    operator = request.args['operator']
-    result = 0
+    operand1 = int('hent ut verdient til operand1')
+    operand2 = int('hen tut verdient til operand2')
+    operator = 'hent ut operator'
+    result = 0  # Utfør riktig operasjon og skriv til result
     # Kode for å gjøre riktig operasjon.
     return "Resultatet er {0}".format(result)
 
